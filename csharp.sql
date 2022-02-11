5d8fbd0cf36c862efed47c1f7596038b60fed3d9
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2022-02-11 17:09:44 -0300

    Adicionando cdados do sql

---
+++ csharp.sql
@@ -0,0 +1,12 @@
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'senha';
+GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE database apps;
+CREATE TABLE aplicativos (
+id INT auto_increment PRIMARY KEY,
+nome text,
+descricao text,
+disponivel_ate text );
+
+INSERT INTO aplicativos (nome, descricao, disponivel_ate ) VALUES ('android para cegos', 'um app do android para cegos', '23 de janeiro');
