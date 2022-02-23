2e33907b785b117cc07b7e8693ce92550ced46ea
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2022-02-23 15:35:22 -0300

    Correções

--- csharp.sql
+++ csharp.sql
@@ -3,11 +3,12 @@ GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE database apps;
+use apps;
 CREATE TABLE aplicativos (
 id INT auto_increment PRIMARY KEY,
 nome text,
 descricao text,
-disponivel_ate date
+disponivel_ate date,
 url VARCHAR(75) );
 
 INSERT INTO aplicativos (nome, descricao, disponivel_ate, url ) VALUES ('android para cegos2', 'um app do android para cegos2','2022-02-02', 'www.androidparacegos.com.br');
